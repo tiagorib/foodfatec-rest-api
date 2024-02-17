76b356f930e18961992712bbb277fc81562925dc
Tiago Ribeiro Carneiro <tiago.rib@gmail.com>
2024-02-17 16:14:55 -0300

    feat: initial commit

---
+++ src/main/java/br/com/fatecrestapi/FoodFatec/FoodFatecApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatecrestapi.FoodFatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFatecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatecrestapi/FoodFatec/FoodFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatecrestapi.FoodFatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
