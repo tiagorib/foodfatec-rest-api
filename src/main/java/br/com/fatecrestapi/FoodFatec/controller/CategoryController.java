984e210250112cc1f52ca6cc8c013359421c2ffc
Tiago Ribeiro Carneiro <tiago.rib@gmail.com>
2024-05-08 19:33:55 -0300

    feat: implements response generic exception

--- src/main/java/br/com/fatecrestapi/FoodFatec/controller/CategoryController.java
+++ src/main/java/br/com/fatecrestapi/FoodFatec/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package br.com.fatecrestapi.FoodFatec.controller;
 
 import br.com.fatecrestapi.FoodFatec.entity.Category;
+import br.com.fatecrestapi.FoodFatec.exception.ResponseGenericException;
 import br.com.fatecrestapi.FoodFatec.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,32 +21,37 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping(value = "/list")
+    @Operation(summary = "List all categories")
     public ResponseEntity<Object> getInfoCategories() {
         List<Category> result = categoryService.getInfoCategories();
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
     @PostMapping(value = "/create")
+    @Operation(summary = "Method for registering category")
     public ResponseEntity<Object> saveCategory(@RequestBody Category category) {
         Category result = categoryService.saveCategory(category);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseGenericException.response(result));
     }
 
     @DeleteMapping(value = "/delete/{idCategory}")
+    @Operation(summary = "Method for delete category")
     public ResponseEntity<Object> deleteCategory(@PathVariable Long idCategory) {
         HashMap<String, Object> result = categoryService.deleteCategory(idCategory);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @GetMapping(value = "/findCategory/{idCategory}")
+    @Operation(summary = "Method for get category by ID")
     public ResponseEntity<Object> getCategoryById(@PathVariable Long idCategory){
         Category result = categoryService.findCategoryById(idCategory);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @PutMapping(value = "/update")
+    @Operation(summary = "Method for update category")
     public ResponseEntity<Object> updateCategory(@RequestBody Category category) {
         Category result = categoryService.updateCategory(category);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
 }
--- src/main/java/br/com/fatecrestapi/FoodFatec/controller/CustomerController.java
+++ src/main/java/br/com/fatecrestapi/FoodFatec/controller/CustomerController.java
@@ -1,10 +1,13 @@
 package br.com.fatecrestapi.FoodFatec.controller;
 
 import br.com.fatecrestapi.FoodFatec.entity.Customer;
+import br.com.fatecrestapi.FoodFatec.exception.ResponseGenericException;
 import br.com.fatecrestapi.FoodFatec.repository.CustomerRepository;
 import br.com.fatecrestapi.FoodFatec.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,57 +25,66 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping(value = "/list")
+    @Operation(summary = "List all customers")
     public ResponseEntity<Object> getInfoCustomers() {
         List<Customer> result = customerService.getInfoCustomers();
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @GetMapping(value = "/findByCpf")
+    @Operation(summary = "Method for find customer by CPF")
     public ResponseEntity<Object> getCustomerByCpf(@RequestBody Customer customer) {
         Optional<Customer> result = customerService.findByCpfCustomer(customer.getCpfCustomer());
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @GetMapping(value = "/findByEmail/{email}")
+    @Operation(summary = "Method for find customer by Email")
     public ResponseEntity<Object> getCustomerByEmail(@PathVariable String email) {
         Optional<Customer> result = customerService.findByEmailCustomer(email);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @GetMapping(value = "/findByStatusTrue")
+    @Operation(summary = "Method for list customers with status true")
     public ResponseEntity<Object> getCustomerByStatusTrue() {
         List<Customer> result = customerService.findByStatusTrue();
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @GetMapping(value = "/findByDateCreated")
+    @Operation(summary = "Method for find customer by Date Created")
     public ResponseEntity<Object> getCustomerByDateCreated(@RequestBody Customer customer) {
         List<Customer> result = customerService.findByDateCreate(customer.getDateCreatedCustomer());
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @PostMapping(value = "/create")
+    @Operation(summary = "Method for registering customer")
     public ResponseEntity<Object> saveCustomer(@RequestBody Customer customer) {
         Customer result = customerService.saveCustomer(customer);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseGenericException.response(result));
     }
 
     @DeleteMapping(value = "/delete/{idCustomer}")
+    @Operation(summary = "Method for delete customer")
     public ResponseEntity<Object> deleteCustomer(@PathVariable Long idCustomer) {
         HashMap<String, Object> result = customerService.deleteCustomer(idCustomer);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @GetMapping(value = "/findCustomer/{idCustomer}")
+    @Operation(summary = "Method for find customer by ID")
     public ResponseEntity<Object> findCustomer(@PathVariable Long idCustomer) {
         Optional<Customer> result = customerService.findCustomerById(idCustomer);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @PutMapping(value = "/update")
+    @Operation(summary = "Method for update customer")
     public ResponseEntity<Object> updateCustomer(@RequestBody Customer customer) {
         Customer result = customerService.updateCustomer(customer);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
 }
